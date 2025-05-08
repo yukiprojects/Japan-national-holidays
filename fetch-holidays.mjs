78ab82a4739c118b4a3ba313f9b467a6c0eae242
yukiprojects <166969423+yukiprojects@users.noreply.github.com>
2025-05-09 01:20:21 +0900

    Update fetch-holidays.mjs

--- fetch-holidays.mjs
+++ fetch-holidays.mjs
@@ -10,7 +10,7 @@ const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURICompone
 fetch(url) // Google Calendar APIを呼び出す
   .then(res => res.json())
   .then(data => {
-    // 受け取ったデータの内容を確認
+    // ここでデータの内容を確認
     console.log(data);
 
     // data.itemsがundefinedの場合のチェック
